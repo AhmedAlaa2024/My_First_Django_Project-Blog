748061c1bd3312dd002815d3fc9eef8d627a60d6
AhmedAlaa2024 <ahmedalaa.com2024@gmail.com>
2020-09-03 04:59:16 +0200

    Changing the views and home page

--- Boards/views.py
+++ Boards/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from .models import Board
 
 # Create your views here.
 
-def Home(request):
+def home(request):
     boards = Board.objects.all()
     return render(request, 'home.html', {'boards': boards})
+
+def board_topics(request, board_name):
+    #A method to get Error404 Not Found Page using Python3's basics.
+    #try:
+    #    board = Board.objects.get(name=board_name)
+    #except Board.DoesNotExist:
+    #    raise Http404
+
+    #A method to get Error404 Page Not Found using django shourtcuts
+    #Note: you should import get_object_or_404 from django.shortcuts
+    board = get_object_or_404(Board, name=board_name)
+
+    return render(request, 'topics.html', {'board': board})
