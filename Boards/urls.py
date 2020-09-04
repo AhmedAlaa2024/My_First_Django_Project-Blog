b2a794711a64b98e388004dc1e4a44951415aa8e
AhmedAlaa2024 <ahmedalaa.com2024@gmail.com>
2020-09-04 02:52:39 +0200

    Create the new topic form and its configurations in the router and views.

--- Boards/urls.py
+++ Boards/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('boards/<board_name>/', views.board_topics, name='board_topics'),
+    path('boards/<board_name>/new/', views.new_topic, name='new_topic'),
 ]
+
--- Boards/views.py
+++ Boards/views.py
@@ -1,22 +1,28 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpRequest
 from .models import Board
 
 # Create your views here.
+
 
 def home(request):
     boards = Board.objects.all()
     return render(request, 'home.html', {'boards': boards})
 
+
 def board_topics(request, board_name):
-    #A method to get Error404 Not Found Page using Python3's basics.
-    #try:
+    # A method to get Error404 Not Found Page using Python3's basics.
+    # try:
     #    board = Board.objects.get(name=board_name)
-    #except Board.DoesNotExist:
+    # except Board.DoesNotExist:
     #    raise Http404
 
-    #A method to get Error404 Page Not Found using django shourtcuts
-    #Note: you should import get_object_or_404 from django.shortcuts
+    # A method to get Error404 Page Not Found using django shourtcuts
+    # Note: you should import get_object_or_404 from django.shortcuts
     board = get_object_or_404(Board, name=board_name)
-
     return render(request, 'topics.html', {'board': board})
+
+
+def new_topic(request, board_name):
+    board = get_object_or_404(Board, name=board_name)
+    return render(request, 'new_topic.html', {'board': board})
