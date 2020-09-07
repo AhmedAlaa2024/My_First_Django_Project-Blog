6d4f9c85adb3ad2bd83ff1bfa26136ff3d1de104
AhmedAlaa2024 <ahmedalaa.com2024@gmail.com>
2020-09-07 17:43:52 +0200

    Adding the <Topic and Post> tables in administration panal.

--- Boards/admin.py
+++ Boards/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Board
+from .models import Board, Topic, Post
 
 # Register your models here.
 @admin.register(Board)
+@admin.register(Topic)
+@admin.register(Post)
 class AuthorAdmin(admin.ModelAdmin):
     pass
